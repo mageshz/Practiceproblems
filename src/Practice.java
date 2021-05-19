e403d3b207e4c649df82f03e57e56125f32c86c4
mageshz <mageshzmoorthy777@gmail.com>
2021-05-19 21:41:37 +0530

    initial pattern commit

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,38 @@
 
 public class Practice {
 
-	//check
+
+public static void printPattern(int n) {
+//  Print the pattern
+//	You a given a number N. You need to print the pattern for the given value of N.
+//	for N = 2 the pattern will be 
+//	2 2 1 1
+//	2 1
+//	for N = 3 the pattern will be 
+//	3 3 3 2 2 2 1 1 1
+//	3 3 2 2 1 1
+//	3 2 1	
+	int numberOfTimes=n;
+	for(int i=1;i<=n;i++) {
+		
+		for(int j=n;j>0;j--) {
+			
+			for(int k=1;k<=numberOfTimes;k++) {
+				
+				System.out.print(""+j);
+
+			}
+		}
+		
+		numberOfTimes=numberOfTimes-1;
+		
+		System.out.println();
+	}
+	
+}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!...");
+		
+			printPattern(3);
 	}
 }
